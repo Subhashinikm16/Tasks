b6450e8237b9c8426d52c2f90778a8843703b1e7
Subhashinikm16 <112954382+Subhashinikm16@users.noreply.github.com>
2022-11-12 16:21:34 +0530

    Update Shift Ope.java

--- Shift Ope.java
+++ Shift Ope.java
@@ -1,10 +1,10 @@
 public class Main {
 	public static void main(String[] args) {
-		int a=5,b=-4;
-		System.out.println("Left Shift:"+(a<<2));
-		System.out.println("Right Shift:"+(a>>2));
-		System.out.println("Unsigned Right Shift:"+(a>>>2));
-		System.out.println("Right Shift:"+(b>>3));
-		System.out.println("Unsigned Right Shift:"+(b>>>3));
+		int number1=5,number2=-4;
+		System.out.println("Left Shift:"+(number1<<2));
+		System.out.println("Right Shift:"+(number1>>2));
+		System.out.println("Unsigned Right Shift:"+(number1>>>2));
+		System.out.println("Right Shift:"+(number2>>3));
+		System.out.println("Unsigned Right Shift:"+(number1>>>3));
     }
 }
